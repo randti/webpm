2d085b402b4cae8ef8bbad4d5fe36ea1debd38d3
unknown <vlasss165@mail.ru>
2022-11-13 14:57:45 +0300

    vlas

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,216 @@
 </script>
 
 <template>
-  <div class="wrapper">Home View</div>
+<div class="home">
+  <h1><p style="text-align: justify; line-height:1;">ИРИТ</p></h1>
+  <h2><p style="text-align: justify; line-height:1;">НГТУ</p></h2>
+
+  <div class="name">
+    <p>Прикладная математика и информатика</p>
+  </div>
+
+  <div class="info">
+    <table style="height: 24px; width: 100%;">
+      <tbody>
+      <tr>
+      <td style="text-align: center;">
+      <table style="height: 188px; width: 100%;" cellpadding="8">
+      <tbody>
+      <tr>
+      <td style="text-align: left; background-color: #696969;"><strong>Направления подготовки</strong></td>
+      <td style="text-align: right; background-color: #696969; padding-left: 60px;">Программирование и моделирование&nbsp;</td>
+      </tr>
+      <tr>
+      <td style="text-align: left; background-color: #808080"><strong>Адрес</strong></td>
+      <td style="text-align: right; background-color: #808080;">
+      <p>Первый корпус, ауд. 1223</p>
+      </td>
+      </tr>
+      <tr>
+      <td style="text-align: left; background-color: #696969;"><strong>Телефон, e-mail</strong></td>
+      <td style="text-align: right; background-color: #696969;">8 (831) 436-63-93,&nbsp;<a href="mailto:filosof6@nntu.nnov.ru">pm@nntu.ru</a>&nbsp;</td>
+      </tr>
+      <tr>
+      <td style="text-align: left; background-color: #808080;"><strong>Почтовый адрес</strong></td>
+      <td style="text-align: right; background-color: #808080;">603950, Н. Новгород, ул.Минина, д.24, ауд. 1223</td>
+      </tr>
+      <tr>
+      <td style="text-align: left; background-color: #696969;"><strong>Заведующий кафедрой</strong></td>
+      <td style="text-align: right; background-color: #696969;">Профессор, доктор физико-математических наук Куркин Андрей Александрович&nbsp;</td>
+      </tr>
+      </tbody>
+      </table>
+      </td>
+      </tr>
+      </tbody>
+      </table>
+  </div>
+  <p>&nbsp;</p>
+
+  <div class="caf">
+    <h3 style="text-align: center; font-family: cursive;"><strong>О кафедре</strong></h3><br>
+  </div>
+
+  <div class="text caf">
+    <p style="text-align: justify; line-height:1.5;">Нашими студентами изучаются такие дисциплины как математический анализ, линейная и высшая алгебра, дифференциальные уравнения, математическая физика, языки и методы программирования, технологии программирования, аппаратные средства ЭВМ, структуры и базы данных, дискретная математика, формальные языки, теория компиляции, численные методы, математические модели окружающей среды, математические модели катастроф, математические модели в экономике и др.</p>
+    <p style="text-align: justify; line-height:1.5;">Обучение ведут преподаватели с многолетним опытом работы, кандидаты и доктора наук, доценты и профессора.</p>
+    <p style="text-align: justify; line-height:1.5;">На кафедре сложились совместные исследовательские группы с университетами Эстонии, Турции, Франции, Великобритании, Австралии.<br> В образовательной области международные контакты поддержаны стипендиями Президента РФ для научных стажировок студентов и аспирантов за рубежом, в рамках которых финансировались стажировки аспирантов кафедры в партнерских университетах Эстонии, Франции и Австралии.</p><br>
+  </div>
+  
+  <p style="text-align: justify; line-height:1.5;">Направления подготовки и специальности:</p><br>
+  
+
+
+
+  <div class="text">
+    <h3 style="text-align: justify;">Бакалавриат</h3>
+
+    <p style="text-align: justify;"><u><strong>Направление&nbsp;подготовки - 01.03.02 «Прикладная математика и информатика»</strong></u></p>
+
+    <ul>
+      <li style="text-align: justify;">Профиль - «Математическое моделирование и компьютерные технологии»</li>
+    </ul>
+
+    <h3 style="text-align: justify;">Магистратура</h3>
+
+    <p style="text-align: justify;"><u><strong>Направление&nbsp;подготовки - 01.04.02 «Прикладная математика и информатика»</strong></u></p>
+
+    <ul>
+      <li style="text-align: justify;">Профиль - «Математическое моделирование»</li>
+    </ul>
+
+    <h3 style="text-align: justify;">Аспирантура</h3>
+
+    <ul>
+      <li style="text-align: justify;">01.00.00 «Математика и механика»</li>
+      <li style="text-align: justify;">02.00.00 «Компьютерные и информационные науки»</li>
+      <li style="text-align: justify;">09.00.00 «Информатика и вычислительная техника»</li>
+    </ul>
+
+    <hr>
+    <p style="text-align: justify;"><strong>Будущие профессии выпускников:</strong></p>
+
+    <ul>
+      <li style="text-align: justify;">программист, инженер-программист;</li>
+      <li style="text-align: justify;">аналитик, системный аналитик;</li>
+      <li style="text-align: justify;">программист-аналитик;</li>
+      <li style="text-align: justify;">математик-программист.</li>
+    </ul>
+
+    <p style="text-align: justify;"><strong>Сферы профессиональной деятельности выпускников:</strong></p>
+
+    <ul>
+      <li style="text-align: justify;">проектирование и реализация комплексных программных систем управления техническими и информационными объектами;</li>
+      <li style="text-align: justify;">математическое моделирование сложных прикладных задач;</li>
+      <li style="text-align: justify;">анализ данных, машинное обучение;</li>
+      <li style="text-align: justify;">управление проектами;</li>
+      <li style="text-align: justify;">преподавание в вузах и др.</li>
+    </ul>
+
+    <p style="text-align: justify;">Прохождение преддипломной практики студентов в большинстве случаев организовано на предприятиях Нижнего Новгорода. Кафедра имеет тесные связи и договора о сотрудничестве с различными предприятиями Нижнего Новгорода.</p>
+
+    <p style="text-align: justify;">Студенты по направлению подготовки «Прикладная математика и информатика» проходят практику на следующих предприятиях: ЗАО «ИНТЕЛ АО», ООО «Датавижн НН», Управление Федерального казначейства по Нижегородской Области, ООО «Мера НН», ООО «Территория контактов», ООО «НИЦ СВТ», ОАО «Гипрогазцентр», ОАО «ФНПЦ «ННИПИ «Кварц», ОАО «ННПО им. М.В.Фрунзе», ООО «Теком», ООО «МФИ Софт», МРСК Центра и приволжья (филиал Нижновэнерго ПО «Цэнтральные электрические сети»), ООО «Нижегородтехэнерго», НИИИС им. Ю.Е. Седакова, ФНЦП ОАО «НПП Полет».</p>
+</div>
+<br>
+
+
+<div class="text">
+  <p style="text-align: justify;">Тематика научных исследований:</p>
+
+  <ol>
+    <li style="text-align: justify;">Проблемы информатики, включая исследования в области создания параллельных алгоритмов.</li>
+    <li style="text-align: justify;">Развитие новых математических моделей в механике, геофизике, физике плазмы, астрофизике.</li>
+    <li style="text-align: justify;">Теоретическая и прикладная математика.</li>
+  </ol>
+
+  <p style="text-align: justify;">Исследования сотрудников кафедры проводятся в рамках совместных проектов по научно-образовательным и исследовательским программам, и поддерживаются грантами Министерства образования и науки Российской Федерации, Федеральной целевой программы «Исследования и разработки по приоритетным направлениям развития научно-технологического комплекса России на 2014-2020 годы», Российского фонда фундаментальных исследований, грантами и стипендиями Президента РФ для молодых российских ученых.</p>
+
+  <p style="text-align: justify;">Ведущие ученые кафедры неоднократно получали премии и звания лауреатов на престижных отечественных и международных конкурсах.</p>
+</div>
+<br>
+
+
+<p><strong>Основные профильные дисциплины:</strong></p>
+<ul>
+	<li>Математическая статистика</li>
+	<li>Языки и методы программирования (Практикум на ЭВМ)</li>
+	<li>Базы данных</li>
+	<li>Численные методы</li>
+	<li>Операционные системы</li>
+	<li>Дискретная математика</li>
+	<li>Дифференциальные уравнения</li>
+	<li>Теория вероятностей</li>
+	<li>Методы оптимизации</li>
+	<li>Безопасность жизнедеятельности</li>
+	<li>Функциональный анализ</li>
+	<li>Комплексный анализ</li>
+	<li>Компьютерная графика</li>
+	<li>Системы программирования</li>
+	<li>Прикладная алгебра</li>
+	<li>Компьютерная математика&nbsp;</li>
+</ul>
+
+
+
+
+
+  
+
+
+
+  
+
+</div>
 </template>
 
-<style scoped lang="scss">
+<style lang="scss">
+@import  '@/assets/scss/_colors.scss';
+
+.text{
+  text-align: justify;
+  line-height:1.5;
+}
+.home{
+  margin-left: 300px;
+  margin-right: 300px;
+
+}
+li {
+  list-style-type: circle;
+
+}
+
+li li {
+  list-style-type: square;
+}
+
+
+h1{
+  margin-left: 10%;
+  margin-right: 10%;
+  font-size: 80px;
+}
+h2{
+  margin-left: 10%;
+  margin-right: 10%;
+  font-size: 50px;
+}
+
+h3{
+  font-size: 30px;
+}
+.name{
+  margin-top: 90px;
+  font-size: 30px;
+}
+strong{
+  font-weight: 700;
+}
+
+.center{
+  margin-left: 50%;
+  margin-right: 50%;
+}
+
+
 </style>
