bcdda4d42d1683f563d347cc8cd048e9cb969190
unknown <randty@yandex.ru>
2022-12-03 20:48:06 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Menu from '@/components/Menu.vue';
+
+</script>
+
+<template>
+  <div id="root">
+    <Menu class="menu" />
+    <RouterView class="router-view" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+#root {
+  display: grid;
+  grid-template-areas: 'router-view';
+  grid-template-columns: auto 1fr;
+  font-family: sans-serif;
+}
+
+.sidebar {
+  grid-area: 'menu';
+}
+
+.router-view {
+  grid-area: 'router-view';
+  padding: 0rem;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="wrapper">История</div>
+</template>
+
+<style scoped lang="scss">
+</style>
