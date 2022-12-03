fdc77694de98accdaca35ff5ae5114a8c20f2133
unknown <randty@yandex.ru>
2022-12-03 20:10:43 +0300

    delete

--- backend/src/main/java/com/example/backend/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
