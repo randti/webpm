22d6a5b1c4fe820b44a3bcb4d2e0d16e24a44442
unknown <randty@yandex.ru>
2022-12-04 21:40:21 +0300

    rebuild

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,16 +1,41 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
 <div class="size">
-Контакты
-<teacher/>
-
+  <p style="margin-top:80px; "></p>
+<div class="Table">
+  <div class="container">
+    <table>
+      <tbody>
+        <tr>
+          <td>Направления подготовки</td>
+          <td>Математическое моделирование</td>
+        </tr>
+        <tr>
+          <td>Адрес</td>
+          <td>Первый корпус, ауд. 1223</td>
+        </tr>
+        <tr>
+          <td>Телефон, e-mail</td>
+          <td>8 (831) 436-63-93, pm@nntu.ru</td>
+        </tr>
+        <tr>
+          <td>Почтовый адрес</td>
+          <td>603950, Н. Новгород, ул.Минина, д.24, ауд. 1223</td>
+        </tr>
+        <tr>
+          <td>Заведующий кафедрой</td>
+          <td>Профессор, доктор физико-математических наук Куркин Андрей Александрович </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </div>
 
 
 
+</div>
 </template>
 
 <script>
@@ -24,4 +49,62 @@ export default {
 };
 </script>
 
-<style></style>
+
+<style scoped lang="scss">
+.Table{
+  html,
+
+  
+  .container {
+    
+    position: static;
+  }
+  
+  table {
+    width: 1100px;
+    border-collapse: collapse;
+    overflow: hidden;
+    border-radius: 25px;
+    margin-left: auto;
+    margin-right: auto;
+    
+  }
+  
+  th,
+  td {
+    padding: 15px;
+    background-color: rgba(88, 86, 86, 0.274);
+    color: auto;
+    font-size:22px;
+  }
+  
+  
+  
+ 
+  
+  tbody {
+    tr {
+      &:hover {
+        background-color: rgba(12, 0, 0, 0.13);
+      }
+    }
+    td {
+      position: relative;
+      &:hover {
+        &:before {
+          content: "";
+          position: absolute;
+          left: 0;
+          right: 0;
+          top: -9999px;
+          bottom: -9999px;
+          background-color: rgba(255,255,255,0.2);
+          z-index: -1;
+        }
+      }
+    }
+  }
+}
+</style>
+
+
